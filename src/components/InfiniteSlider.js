4cc8e65316d39be44dc4c60920ad23fe6e9ad58e
Sachin <wwwsachinsingh815@gmail.com>
2025-04-15 02:56:57 +0530

    top slider fixed

--- src/components/InfiniteSlider.js
+++ src/components/InfiniteSlider.js
@@ -10,23 +10,29 @@ const InfiniteSlider = ({ images }) => {
   useEffect(() => {
     const container = containerRef.current;
     if (!container) return;
-
+  
     const scrollStep = 4;
     let scrollAmount = 0;
-
-    const loop = () => {
-      scrollAmount += scrollStep;
-
-      if (scrollAmount >= container.scrollWidth * 0.5) {
-        scrollAmount = scrollAmount - container.scrollWidth * 0.5;
-      }
-
-      container.scrollLeft = scrollAmount;
-      requestAnimationFrame(loop);
+  
+    const startLoop = () => {
+      const scrollLoop = () => {
+        scrollAmount += scrollStep;
+  
+        const resetPoint = container.scrollWidth / 2;
+        if (scrollAmount >= resetPoint) {
+          scrollAmount = 4;
+        }
+  
+        container.scrollLeft = scrollAmount;
+        requestAnimationFrame(scrollLoop);
+      };
+  
+      requestAnimationFrame(scrollLoop);
     };
 
-    requestAnimationFrame(loop);
+    setTimeout(startLoop, 100);
   }, []);
+  
 
   return (
     <div className={styles.sliderWrapper} ref={containerRef}>
@@ -37,6 +43,7 @@ const InfiniteSlider = ({ images }) => {
             width={500}
             height={500}
             src={src}
+            priority
             alt={`img-${idx}`}
             draggable={false}
           />
